b0ba83dc954b2f079979f786fff7780c60bd22a3
yubinx <yubinxie222@gmail.com>
2025-02-15 20:18:45 -0500

    Write functions that print number from 0-9

---
+++ functions.py
@@ -0,0 +1,11 @@
+def printnumbers():
+    for i in range(10):
+        print(i)
+
+
+def __main__():
+    print("Hello World")
+    print("My Name is Yubin")
+    printnumbers()
+    
+__main__() # This will call the __main__() function in functions.py
